508b00981c2c5185d266a8367f731a54a5c15a74
George Redinger <george@georgeredinger.com>
2012-05-10 12:19:48 -0700

    typo riding --> rising

--- current_daemon.rb
+++ current_daemon.rb
@@ -6,30 +6,30 @@ def init
 	data_bits = 8
 	stop_bits = 1
 	parity = SerialPort::NONE
-  @rising_edge=0;
+	@rising_edge=0;
 	@start_brew=0
 	@finish_brew=0
 	if ARGV.length > 0
 		@sp = File.open(ARGV[0],"r")
 		@test=true
 	else
-	  @sp = SerialPort.new(port_str, baud_rate, data_bits, stop_bits, parity)
+		@sp = SerialPort.new(port_str, baud_rate, data_bits, stop_bits, parity)
 		@test=false
 	end 
 end
 
 def tweet(what)
 	if @test
-		puts "Fresh pot"
+		puts "#{what}"
 	else
-         `twurl -d 'status=Fresh Pot' /1/statuses/update.xml`
+		`twurl -d "status=#{what}" /1/statuses/update.xml`
 	end
 end
 
 def sample
 	if @test
 		line=@sp.gets.split
-	  return [line[0].to_f,line[1].to_f] 
+		return [line[0].to_f,line[1].to_f] 
 	end
 
 	while true 
@@ -52,19 +52,19 @@ def main
 		current=sample
 		if current[1] > 0.5 
 			if heating == 0 
-		   	puts "#{current[0]} #{heating}"
+				puts "#{current[0]} #{heating}"
 			end
 			heating+=1
-			riding_edge = true
+			rising_edge = true
 			falling_edge = false
 		end
 		if current[1] < 0.25
 			if heating >=1
 				falling_edge=true
-				riding_edge=false
+				rising_edge=false
 				puts "#{current[0]} #{heating}"
 				if heating > 500
-          tweet('Fresh Pot')
+					tweet('Fresh Pot')
 				end
 				heating=0
 			end
