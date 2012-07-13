c4d1bb6fa45de0c595f63641925cd1772b4976c5
George Redinger <george@georgeredinger.com>
2012-07-13 12:04:10 -0700

    quotes

--- notify.sh
+++ notify.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 d=`date`
-twurl -d \"status=$d Fresh Pot\" /1/statuses/update.xml > /dev/null
+twurl -d "status=$d Fresh Pot" /1/statuses/update.xml > /dev/null
 mpg123 bell.mp3
 exit(0)
 
